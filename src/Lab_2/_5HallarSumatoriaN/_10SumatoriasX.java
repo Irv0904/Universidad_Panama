6ed422b58510467788ac14ee477998384dae7550
irving-09 <irvingvillarreal883@gmail.com>
2023-09-24 23:36:07 -0500

    Se agrego mas tarea de guardado, por favor revisar si estan bien

---
+++ src/Lab_2/_5HallarSumatoriaN/_10SumatoriasX.java
@@ -0,0 +1,31 @@
+package Lab_2._5HallarSumatoriaN;
+
+import java.io.IOException;
+
+public class _10SumatoriasX
+{
+    static int n;
+    static double sum;
+
+    static double Sumatoria (int N)
+    {
+        double s = 0;
+
+        int c;
+
+        for (c = 1 ; c <= N ; c++)
+        {
+            s = s + 1 / (2 * (double) c);
+            System.out.print ("1/" + (c*3) + " + ");
+        }
+        return s;
+    }
+    public static void main (String[]  args) throws IOException
+    {
+        int n = 4;
+        System.out.println (" SUMATORIA DE 1/2+ 1/4 + 1/6+ 1/8 + ...");
+        System.out.println ("Cuantos terminos desea sumar....");
+        sum = Sumatoria (n);
+        System.out.println ("\n\n La suma es " + sum);
+    }
+}
---
+++ src/Lab_2/_5HallarSumatoriaN/_8SumatoriasVIII.java
@@ -0,0 +1,35 @@
+package Lab_2._5HallarSumatoriaN;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class _8SumatoriasVIII
+{
+    static int n;
+    static double sum;
+
+    static double Sumatoria (int N)
+    {
+        double s = 0;
+
+        int c;
+        int a = 1;
+
+        for (c = 2 ; c <= N ; c++)
+        {
+            s = s + 1 / ((double) c);
+            System.out.print (a+"/" + (c) + " + ");
+            a++;
+        }
+        return s;
+    }
+    public static void main (String[]  args) throws IOException
+    {
+        int n = 6;
+        System.out.println (" SUMATORIA DE 1/2+ 1/4 + 1/6+ 1/8 + ...");
+        System.out.println ("Cuantos terminos desea sumar....");
+        sum = Sumatoria (n);
+        System.out.println ("\n\n La suma es " + sum);
+    }
+}
